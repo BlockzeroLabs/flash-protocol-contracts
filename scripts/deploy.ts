6bc12619678680331fbe8150e4b895a0b0f077c9
shakeib98 <shakeib98@outlook.com>
2020-11-25 12:12:17 +0500

    refactor: shift to hardhat

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,22 @@
-// We require the Buidler Runtime Environment explicitly here. This is optional
+// We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
-// When running the script with `buidler run <script>` you'll find the Buidler
+// When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "@nomiclabs/buidler";
+import { ethers } from "hardhat";
 import { Contract, ContractFactory } from "ethers";
 
 async function main(): Promise<void> {
-  // Buidler always runs the compile task when running scripts through it.
+  // Hardhat always runs the compile task when running scripts through it.
   // If this runs in a standalone fashion you may want to call compile manually
   // to make sure everything is compiled
   // await run("compile");
 
   // We get the contract to deploy
-  const FlashProtocol: ContractFactory = await ethers.getContractFactory("FlashProtocol");
-  const flashProtocol: Contract = await FlashProtocol.deploy();
+  const FlashProtocolFactory: ContractFactory = await ethers.getContractFactory("FlashProtocol");
+  const flashProtocol: Contract = await FlashProtocolFactory.deploy();
   await flashProtocol.deployed();
 
-  console.log("FlashProtocol deployed to: ", flashProtocol.address);
+  console.log("Greeter deployed to: ", flashProtocol.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
