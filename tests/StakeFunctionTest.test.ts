7816c2e1ad4772fc49542e00ac2b4f89f4507c35
shakeib98 <shakeib98@outlook.com>
2021-03-13 13:25:13 +0500

    fix: unit test review

--- tests/StakeFunctionTest.test.ts
+++ tests/StakeFunctionTest.test.ts
@@ -12,6 +12,7 @@ import stakes from "../data.json"
 import { constants, ethers, utils } from "ethers";
 
 use(solidity)
+use(require('chai-bignumber')())
 
 describe("Unit tests", function () {
     let flashToken: FlashTokenTest;
@@ -21,7 +22,7 @@ describe("Unit tests", function () {
     let walletReceiverStake: SignerWithAddress;
     let totalSupply = "10000000000000000000000000"
     let deployFlag: boolean = false;
-    let PRECISION = "1000000000000000000"
+    let PRECISION = new BigNumber("1000000000000000000")
     let ONE_YEAR_SECONDS = "31536000"
 
     before(async function () {
@@ -47,23 +48,20 @@ describe("Unit tests", function () {
             let lockedAmount = (await flashToken.balanceOf(flashProtocol.address)).toString();
 
             let totalFlash = new BigNumber(lockedAmount).plus(new BigNumber(amountIn)).multipliedBy(PRECISION).toFixed(0)
-            return new BigNumber(PRECISION).minus(new BigNumber(totalFlash).dividedBy(new BigNumber(totalSupply))).dividedBy(2).toFixed(0)
+            return PRECISION.minus(new BigNumber(totalFlash).dividedBy(new BigNumber(totalSupply))).dividedBy(2).toFixed(0)
         }
 
         async function getMintedAmount(amountIn: string, expiry: string, fpy: string) {
-            return (new BigNumber(amountIn).multipliedBy(new BigNumber(expiry)).multipliedBy(new BigNumber(fpy))).dividedBy(new BigNumber(PRECISION).multipliedBy(new BigNumber(ONE_YEAR_SECONDS))).toFixed(0)
+            return (new BigNumber(amountIn).multipliedBy(new BigNumber(expiry)).multipliedBy(new BigNumber(fpy))).dividedBy(PRECISION.multipliedBy(new BigNumber(ONE_YEAR_SECONDS))).toFixed(0)
         }
 
         async function matchAmount(mintedAmount: string) {
-            return (new BigNumber(mintedAmount).multipliedBy(new BigNumber(2000)).dividedBy(new BigNumber(10000))).toFixed(0)
+            console.log(mintedAmount)
+            return (new BigNumber(mintedAmount).multipliedBy(new BigNumber("2000")).dividedBy(new BigNumber("10000"))).toFixed(0)
         }
 
-        async function calculateUnstakeAount() {
-
-        }
-
-        async function calculateBurnAmount() {
-
+        async function calculateBurnAmount(amountIn: string, remaining: string, totalTime: string): Promise<string> {
+            return new BigNumber(amountIn).multipliedBy(new BigNumber(remaining)).dividedBy(new BigNumber(totalTime)).toFixed(0);
         }
 
         async function stake(amount: BigNumber, duration: string, mintedAmount: string, fpyAfterStake: string): Promise<any> {
@@ -74,67 +72,76 @@ describe("Unit tests", function () {
 
                 let balanceStakeReceiverBefore = (await flashToken.balanceOf(walletReceiverStake.address)).toString()
                 let balanceMatchReceiverBefore = (await flashToken.balanceOf(walletMatchReceiver.address)).toString()
-                
+
                 let fpyBeforeStake = await getFpy(amount.toString());
                 let fpyBeforeStakeContract = new BigNumber((await flashProtocol.getFPY(amount.toString())).toString()).toFixed(0)
                 expect(fpyBeforeStake).to.be.equal(fpyBeforeStakeContract)
 
                 const protocol = flashProtocol.connect(wallet);
                 let tx = await protocol.stake(amount.toString(), duration, walletReceiverStake.address, "0x");
-                
+
                 let balancerStakeReceiverAfter = (await flashToken.balanceOf(walletReceiverStake.address)).toString()
                 let balanceMatchReceiverAfter = (await flashToken.balanceOf(walletMatchReceiver.address)).toString();
-                
+
                 let fpyAfterStake = await getFpy("0");
                 let fpyAfterStakeContract = new BigNumber((await flashProtocol.getFPY("0")).toString()).toFixed(0);
                 console.log(fpyAfterStake, fpyAfterStakeContract)
                 expect(fpyAfterStake).to.be.equal(fpyAfterStakeContract);
-                
+
                 let mintedAmount = await getMintedAmount(amount.toString(), duration, fpyBeforeStake);
                 let differenceBalance = (new BigNumber(balancerStakeReceiverAfter).minus(new BigNumber(balanceStakeReceiverBefore))).toFixed(0)
                 console.log(mintedAmount, differenceBalance, "Minted amount");
                 expect(mintedAmount).to.be.equal(differenceBalance);
 
-                // let mintedAmount = await matchAmount(amount.toString(), duration, fpyBeforeStake);
-                // let differenceBalance = (new BigNumber(balancerStakeReceiverAfter).minus(new BigNumber(balanceStakeReceiverBefore))).toFixed(0)
-                // console.log(mintedAmount, differenceBalance, "Minted amount");
-                // expect(mintedAmount).to.be.equal(differenceBalance);
+                let matchAmountMinted = await matchAmount(mintedAmount.toString());
+                let differenceMatchAmount = (new BigNumber(balanceMatchReceiverAfter).minus(new BigNumber(balanceMatchReceiverBefore))).toFixed(0)
+                console.log(matchAmountMinted, differenceMatchAmount, "Minted amount");
+                expect(matchAmountMinted).to.be.equal(differenceMatchAmount);
 
-                // let totalSupplyContract = (await flashToken.totalSupply()).toString()
-                // let mintedAmountContract = (new BigNumber(totalSupplyContract).minus(new BigNumber(totalSupply))).toFixed(0).toString();
-                // expect(mintedAmountContract.toString()).to.be.equal(mintedAmount)
-                return null
+                return tx
             }
             catch (e) {
                 return false
             }
         }
 
-        async function unstakeEarly(id: string, unstakeAmount: string, stakeAmount: string): Promise<any> {
-            let stake = (await flashProtocol.stakes(id))
-            console.log(stake.expireAfter.toString(), "EXPIREAFTER")
-            let stakeAmountContract = stake.amountIn.toString()
-            let balanceOfUserContract = (await flashToken.balanceOf(wallet.address)).toString();
+        async function unstakeEarly(id: string, timestamp: Number): Promise<any> {
+            let stakeBefore = (await flashProtocol.stakes(id))
+
+            let burnAmountCalculate = await calculateBurnAmount(stakeBefore.amountIn.toString(), new BigNumber(new BigNumber(stakeBefore.expireAfter.toString())).minus(timestamp.toString()).toFixed(0), new BigNumber(stakeBefore.expiry.toString()).toFixed(0))
+
+            let stakeAmountContractBefore = stakeBefore.amountIn.toString()
+            let unstakeAmount = new BigNumber(stakeAmountContractBefore).minus(burnAmountCalculate).toFixed(0);
+            let balanceOfUserContractBefore = (await flashToken.balanceOf(wallet.address)).toString();
+
             const protocol = flashProtocol.connect(wallet);
             let tx = await protocol.unstakeEarly(id);
             let receipt = await hre.ethers.provider.getTransaction(tx.hash)
             let block = await hre.ethers.provider.getBlock(String(receipt.blockHash))
-            console.log(block.timestamp, "TIMESTAMP")
+            console.log(block.timestamp, timestamp)
+
             let balanceOfAfterUnstake = (await flashToken.balanceOf(wallet.address)).toString();
-            let differenceReceive = (new BigNumber(balanceOfAfterUnstake).minus(new BigNumber(balanceOfUserContract))).toFixed(0).toString();
-            let burnAmount = (new BigNumber(stakeAmountContract).minus(new BigNumber(differenceReceive))).toFixed(0).toString()
-            expect(burnAmount).to.be.equal(unstakeAmount);
+            let differenceReceive = (new BigNumber(balanceOfAfterUnstake).minus(new BigNumber(balanceOfUserContractBefore))).toFixed(0)
+            expect(differenceReceive).to.be.equal(unstakeAmount);
             return tx
         }
 
-        async function unstake(id: string, fpyAfterUnstake: string): Promise<any> {
+        async function unstake(id: string): Promise<any> {
+            let balanceUserBefore = (await flashToken.balanceOf(wallet.address)).toString()
+            let stakeBefore = (await flashProtocol.stakes(id))
+            let stakeBeforeAmount = new BigNumber(stakeBefore.amountIn.toString()).toFixed(0)
+
+            console.log(stakeBefore.staker.toString())
+
             const protocol = flashProtocol.connect(wallet);
             let tx = await protocol.unstake(id);
-            let stake = (await flashProtocol.stakes(id))
-            let stakeAmountContract = stake.amountIn.toString()
+            let stakeAfter = (await flashProtocol.stakes(id))
+            let stakeAmountContract = stakeAfter.amountIn.toString()
             expect(stakeAmountContract).to.be.equal("0")
-            let fpyAfterUnstakeContract = (await flashProtocol.getFPY("10000000000000000000000")).toString();
-            expect(fpyAfterUnstakeContract).to.be.equal(fpyAfterUnstake);
+
+            let balanceUserAfter = (await flashToken.balanceOf(wallet.address)).toString()
+            let balanceDifferece = new BigNumber(balanceUserAfter).minus(balanceUserBefore).toFixed(0);
+            expect(balanceDifferece).to.be.equal(stakeBeforeAmount);
             return tx
         }
 
@@ -151,7 +158,7 @@ describe("Unit tests", function () {
                 [
                     amount,
                     duration,
-                    wallet.address,
+                    walletReceiverStake.address,
                     wallet.address,
                     block.timestamp
                 ]
@@ -184,17 +191,17 @@ describe("Unit tests", function () {
             if (data.unstakeTime !== '0') {
                 deployFlag = false;
                 let tx = await stake(data.stakeAmount, data.expiry, data.mintedAmount, data.fpyAfterStake)
-                // if (tx) {
-                //     let obj = await generateId(data.stakeAmount, data.expiry, tx)
-                //     if (data.expiry === data.unstakeTime) {
-                //         await hre.network.provider.send("evm_setNextBlockTimestamp", [obj.timestamp + Number(data.unstakeTime)])
-                //         // await hre.network.provider.send("evm_increaseTime", [Number(data.unstakeTime)])
-                //         console.log(data.stakeAmount)
-                //         await unstake(obj.id, data.fpyAfterUnstake);
-                //     } else {
-                //         await unstakeEarly(obj.id, data.unstakeAmount, data.stakeAmount)
-                //     }
-                // }
+                if (tx) {
+                    let obj = await generateId(data.stakeAmount, data.expiry, tx)
+                    await hre.network.provider.send("evm_setNextBlockTimestamp", [obj.timestamp + Number(data.unstakeTime)])
+                    if (data.expiry === data.unstakeTime) {
+                        // await hre.network.provider.send("evm_increaseTime", [Number(data.unstakeTime)])
+                        console.log(obj.id)
+                        await unstake(obj.id);
+                    } else {
+                        await unstakeEarly(obj.id, obj.timestamp + Number(data.unstakeTime))
+                    }
+                }
             } else {
                 await stake(data.stakeAmount, data.expiry, data.mintedAmount, data.fpyAfterStake)
             }
